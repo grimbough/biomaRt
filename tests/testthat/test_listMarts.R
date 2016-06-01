bab9f1e5a32d386173da1c9119e42f7f4bbb4e01
Mike Smith <grimbough@gmail.com>
2016-06-01 18:49:03 +0200

    Updated test

--- tests/testthat/test_listMarts.R
+++ tests/testthat/test_listMarts.R
@@ -2,4 +2,4 @@ library(biomaRt)
 context('Testing listMarts() function')
 
 ## we expect this to error as the URL is out of date.
-expect_error(useMart("ENSEMBL_MART_ENSEMBL",dataset="mmusculus_gene_ensembl", host="www.biomart.org"), "Error: Unable to process the returned data")
+expect_error(useMart("ENSEMBL_MART_ENSEMBL",dataset="mmusculus_gene_ensembl", host="www.biomart.org"), "Unable to determine the list of available marts")
