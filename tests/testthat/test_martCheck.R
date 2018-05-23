ebf06a86bb44cd1440f50e195cd99dad7fbfc35e
Mike Smith <grimbough@gmail.com>
2018-05-23 17:16:04 +0200

    Small update to unit tests

--- tests/testthat/test_martCheck.R
+++ tests/testthat/test_martCheck.R
@@ -1,12 +1,17 @@
-# library(biomaRt)
-# context('Testing martCheck function')
-# 
-# expect_error(biomaRt:::martCheck())
-# expect_error(biomaRt:::martCheck("INVALID_OBJECT"))
-# 
-# ensembl=useMart("ensembl")
-# expect_error(biomaRt:::martCheck(ensembl), "No dataset selected, please select a dataset first")
-# 
-# ensembl = useDataset("hsapiens_gene_ensembl",mart=ensembl)
-# expect_error(biomaRt:::martCheck(ensembl, biomart = "Not_real_mart"), "This function only works when used with the Not_real_mart BioMart")
-# expect_silent(biomaRt:::martCheck(ensembl, biomart = "ENSEMBL_MART_ENSEMBL"))
+library(biomaRt)
+context('Testing martCheck function')
+
+test_that("martCheck() catches bad input", { 
+    expect_error(biomaRt::martCheck())
+    expect_error(biomaRt::martCheck("INVALID_OBJECT"))
+    
+    ensembl <- useMart("ensembl")
+    expect_error(biomaRt::martCheck(ensembl), "No dataset selected, please select a dataset first")
+    
+    ensembl <- useDataset("hsapiens_gene_ensembl",mart=ensembl)
+    expect_error(biomaRt::martCheck(ensembl, biomart = "Not_real_mart"), "This function only works when used with the Not_real_mart BioMart")
+})
+
+test_that('martCheck() is quite for valid input', {
+    expect_silent(biomaRt:::martCheck(ensembl, biomart = "ENSEMBL_MART_ENSEMBL"))
+})
