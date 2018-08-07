b011120eef87e726db9b6fa5d28dbaca073f9408
Mike Smith <grimbough@gmail.com>
2018-08-07 13:40:24 +0200

    Added positive test for getLDS

--- tests/testthat/test_getLDS.R
+++ tests/testthat/test_getLDS.R
@@ -1,14 +1,25 @@
 library(biomaRt)
 context('getLDS() function')
 
-ensembl <- useMart("ENSEMBL_MART_ENSEMBL", 
+ensembl_hsapiens <- useMart("ENSEMBL_MART_ENSEMBL", 
                 dataset="hsapiens_gene_ensembl")
 plants <- useMart("plants_mart", host="plants.ensembl.org", 
                  dataset="athaliana_eg_gene")
+ensembl_rnorvegicus <- useMart("ENSEMBL_MART_ENSEMBL", 
+                       dataset="rnorvegicus_gene_ensembl")
 
 test_that("Error with separate hosts", { 
-    expect_error(getLDS(attributes="ensembl_gene_id", mart=ensembl, 
+    expect_error(getLDS(attributes="ensembl_gene_id", mart=ensembl_hsapiens, 
                         attributesL="ensembl_gene_id", martL=plants),
                  regexp = 'Both datasets must be located on the same host')
 })
 
+
+test_that("Find human/rat homologs", { 
+    
+    expect_is(x <- getLDS(attributes="ensembl_gene_id", mart=ensembl_hsapiens, 
+                          filters = "ensembl_gene_id", values = "ENSG00000084453",
+                          attributesL="ensembl_gene_id", martL=ensembl_rnorvegicus),
+              "data.frame")
+    expect_true("ENSRNOG00000047493" %in% x[,2])
+})
