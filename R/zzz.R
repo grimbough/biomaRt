0af71a2f7e68891ef0280a48823097403102a33e
Mike Smith <grimbough@gmail.com>
2020-10-09 17:28:19 +0200

    Improve detection of when to change cipher list

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,12 @@
 .onLoad <- function(libname, pkgname) {
-    
+
     ## This address problems with Ubuntu 20.04 and the Ensembl https certificates
-    if( Sys.info()["sysname"] == "Linux" ) {
-        httr_config <- httr::config(ssl_verifypeer = FALSE, ssl_cipher_list = "DEFAULT@SECLEVEL=1")
+    test <- try(httr::GET("https://www.ensembl.org"), silent = TRUE)
+    if(inherits(test, "try-error")) {
+      if(grepl(test[1], pattern = "sslv3 alert handshake failure")) {
+        httr_config <- httr::config(ssl_verifypeer = FALSE, 
+                                    ssl_cipher_list = "DEFAULT@SECLEVEL=1")
         httr::set_config(httr_config)
+      }
     }
-    
 }
