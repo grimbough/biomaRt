1e7464d3ce60da165f1f3fa125d4fc7715c0fa3f
Mike Smith <grimbough@gmail.com>
2020-11-11 11:53:12 +0100

    Improved detection and handling of Ensembl SSL issues

--- R/zzz.R
+++ R/zzz.R
@@ -1,32 +1,35 @@
-.onLoad <- function(libname, pkgname) {
+
   
-  default_sec <- FALSE
-  
-  ## This address problems with Ubuntu 20.04 and the Ensembl https certificates
-  test <- try(httr::GET("https://www.ensembl.org"), silent = TRUE)
-  if(inherits(test, "try-error")) {
-    if(grepl(test[1], pattern = "sslv3 alert handshake failure")) {
-      default_sec <- TRUE
-      new_config <- httr::config(ssl_cipher_list = "DEFAULT@SECLEVEL=1")
-      httr::set_config(new_config, override = TRUE)
-    }
-  }
-  
-  ##  Try to catch problems with trusting the Ensembl certificate.
-  test <- try(httr::GET("https://www.ensembl.org"), silent = TRUE)
-  if(inherits(test, "try-error")) {
-    ## users have reported two error messages for this
-    if(grepl(x = test[1], 
-             pattern = "(unable to get local issuer certificate)|(server certificate verification failed)")) {
-      if(default_sec) {
-        new_config <- httr::config(ssl_verifypeer = FALSE, 
-                                   ssl_cipher_list = "DEFAULT@SECLEVEL=1")
-      } else {
+.test_ensembl <- function() {
+  httr::GET("https://www.ensembl.org/index.html", timeout(3), set_cookies(redirect_mirror = "no"))
+  httr::GET("https://uswest.ensembl.org/index.html", timeout(3), set_cookies(redirect_mirror = "no"))
+}
+
+.check_ensembl_ssl <- function() {
+  for(i in seq_len(2)) {
+    test <- try(.test_ensembl(), silent = TRUE)
+    
+    if(inherits(test, "try-error")) {
+      
+      if(grepl(test[1], ## This address problems with Ubuntu 20.04 et al and the Ensembl https certificates
+               pattern = "sslv3 alert handshake failure")) {
+        new_config <- httr::config(ssl_cipher_list = "DEFAULT@SECLEVEL=1")
+        
+      } else if(grepl(x = test[1], ## two reported error messages solved with the same setting
+                      pattern = "(unable to get local issuer certificate)|(server certificate verification failed)")) {
         new_config <- httr::config(ssl_verifypeer = FALSE)
       }
-      verify_peer <- TRUE
       httr::set_config(new_config, override = FALSE)
+    } else {
+      ## no need to loop twice if there's no error
+      break;
     }
   }
+}
   
+
+.onLoad <- function(libname, pkgname) {
+
+  .check_ensembl_ssl()
+
 }
