cb15ebc8132ac800ba7459ec04eb8c5cb48c245a
Mike Smith <grimbough@gmail.com>
2021-01-22 16:39:24 +0100

    Fix infinite loop in mocked test

--- tests/testthat/test_zzz.R
+++ tests/testthat/test_zzz.R
@@ -5,7 +5,7 @@ test_that("Both SSL settings are applied if needed", {
     
     m <- mock(stop("sslv3 alert handshake failure"),
               stop("unable to get local issuer certificate"), 
-              cycle = TRUE)
+              "")
     stub(.check_ensembl_ssl, ".test_ensembl", m)
     
     expect_message(.check_ensembl_ssl())
@@ -44,7 +44,8 @@ test_that("If we hit an unknown error", {
     ## clear any httr configuration before running these tests
     httr::reset_config()
     
-    m <- mock(stop("This is an unexpected SSL error"), cycle = TRUE)
+    m <- mock(stop("This is an unexpected SSL error"), 
+              "")
     stub(.check_ensembl_ssl, ".test_ensembl", m)
     
     expect_message(.check_ensembl_ssl(), "Unknown error encountered")
