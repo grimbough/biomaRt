7b25f94b2adfe88fa9bf2704a9a3a6f23e4f9238
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-08-18 14:46:30 +0200

    Fix biomartCache manually

--- R/biomaRt-package.R
+++ R/biomaRt-package.R
@@ -16,24 +16,6 @@
 #' @name biomaRt-deprecated
 NULL
 
-
-#' biomaRt result caching
-#'
-#' biomaRt makes use of a results cache to speedup execution of queries that
-#' have been run before. These functions provide details on the status of this
-#' cache, and allow it to be deleted.
-#'
-#'
-#' @aliases biomartCacheInfo biomartCacheClear
-#' @return These functions do not return anything and are called for their side
-#' effects.  \code{biomartCacheInfo()} prints the location of the cache, along
-#' with the number of files and their total size on disk.
-#' \code{biomartCacheClear()} will delete the current contents of the cache.
-#' @author Mike Smith
-#' @keywords IO
-NULL
-
-
 #' Class Mart
 #'
 #' Represents a Mart class, containing connections to different BioMarts
