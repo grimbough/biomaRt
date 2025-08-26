e93e00b00509e3b92e2d1628d6112c93acba6057
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-08-26 16:32:58 +0200

    Convert roxygen documentation to markdown syntax (#135)

--- R/biomaRt-package.R
+++ R/biomaRt-package.R
@@ -7,11 +7,9 @@
 #' The following functions are defunct and no longer work; use the replacement
 #' indicated below:
 #'
-#' \itemize{
-#' \item{filterOptions: \code{\link{listFilterOptions}}}
-#' \item{listFilterValues: \code{\link{listFilterOptions}}}
-#' \item{searchFilterValues: \code{\link{searchFilterOptions}}}
-#' }
+#' * filterOptions: [listFilterOptions()]
+#' * listFilterValues: [listFilterOptions()]
+#' * searchFilterValues: [searchFilterOptions()]
 #'
 #' @name biomaRt-deprecated
 #' @aliases filterOptions searchFilterValues listFilterValues
